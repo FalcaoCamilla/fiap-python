edfd9755df6fd8ebb1950805d8de7977e029442f
FalcaoCamilla <millafalcao.acad@gmail.com>
2023-04-02 12:05:10 -0300

    Capítulo 3 | Atividade (Q03)

--- cap03/atividade/RM552230_EX03.py
+++ cap03/atividade/RM552230_EX03.py
@@ -4,11 +4,11 @@ media_par = 0
 media_impar = 0
 
 for i in range (1, 51, 1):
-    if i % 2 == 0:
-        nota_par += float(input(f"Você está digitando as notas dos alunos pares\nPor favor, insira a nota do aluno de número {i}\n"))
-for i in range (1, 51, 1):
     if i % 2 == 1:
         nota_impar += float(input(f"Você está digitando as notas dos alunos impares\nPor favor, insira a nota do aluno de número {i}\n"))
+for i in range (1, 51, 1):
+    if i % 2 == 0:
+        nota_par += float(input(f"Você está digitando as notas dos alunos pares\nPor favor, insira a nota do aluno de número {i}\n"))
 
 media_par = nota_par/25
 media_impar = nota_impar/25

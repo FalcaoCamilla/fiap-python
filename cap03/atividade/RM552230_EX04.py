9d75e56d507335bfc48ca78c1aa703337e2ae3a1
FalcaoCamilla <millafalcao.acad@gmail.com>
2023-04-09 19:28:16 -0300

    Reestruturação do repositório

--- cap03/atividade/RM552230_EX04.py
+++
@@ -1,9 +0,0 @@
-minutos = int(input("Digite o número de minutos atual: "))
-
-fatorial = 1
-for i in range(1, minutos+1):
-    fatorial *= i
-
-senha = "LIBERDADE" + str(fatorial)
-
-print("A senha necessária para desbloqueio é:", senha)
